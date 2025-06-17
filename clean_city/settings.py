6f826708f5a4ab83ce302ea3ccb90aee67b6557b
Vadim Shlykov <shlikov.vadim@gmail.com>
2025-06-17 21:32:10 +0300

    update settings

--- clean_city/settings.py
+++ clean_city/settings.py
@@ -112,6 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = (BASE_DIR / STATIC_URL,)
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

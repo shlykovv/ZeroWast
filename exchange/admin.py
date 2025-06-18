722144b489f3490a3534aa554634e6bab9b726d4
shlykovv <shlikov.vadim@gmail.com>
2025-06-18 23:53:56 +0300

    Добавлена модель Category и связана с моделью ExchangeItem для удобного управления, модель Cftegory зарегестрирована в admin.py и добавлена в serilizer, сделан в serializer вывод категории через метод __str__ с помощью StringRelatedField

--- exchange/admin.py
+++ exchange/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from exchange.models import ExchangeItem
+from exchange.models import ExchangeItem, Category
 
 
 admin.site.register(ExchangeItem)
+admin.site.register(Category)

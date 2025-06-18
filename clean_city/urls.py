1060590bb64be387e5b889013f397eecb38c4f27
shlykovv <shlikov.vadim@gmail.com>
2025-06-18 22:16:00 +0300

    Update urls.py and settings.py

--- clean_city/urls.py
+++ clean_city/urls.py
@@ -32,6 +32,12 @@ router.register(r'exchange-items', ExchangeItemViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path(
+        'api/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'),
+    path(
+        'api/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh')
 ]
